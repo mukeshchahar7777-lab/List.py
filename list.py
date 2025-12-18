920a778ca10e6cf4d02c7a3faae56a2e145a63b5
mukeshchahar7777-lab <mukeshchahar7777@gmail.com>
2025-12-18 14:14:32 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+#list = [10 , 20] , [30 , 40] , [50 , 60]
+list_2 =[10 , 20, 30, "mukku","chahar"]
+#print(list)
+#print(list_2)
+#print(list[1])        # indexing 
+#print(list_2[3])     #indexing.....
+#print(list[0:1])      # slicing....
+#print(list_2[0:5])   #slicing......
+
+#print("\n")
+
+#print("\n     method........") 
+#print(list_2.sum())
+#print(list_2.count()'')
+print(list_2.insert("mukku"))
+
+#print()
+
+
